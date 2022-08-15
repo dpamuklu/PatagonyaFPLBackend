11e036a3d38dba94a5ec57fa500303dc6dc3358b
dpamuklu <doganpamuklu05@gmail.com>
2022-08-15 13:13:44 +0300

    job test

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const axios = require("axios");
 app.use(cors());
 app.use(express.json());
 const baseURL = "https://draft.premierleague.com/api/league/99028/details";
+const jobURL = baseURL + "/job";
 app.get("/", async (req, res) => {
   const response = await axios.get(baseURL);
+  console.log("test");
   res.status(200).json(response.data);
 });
+
+app.get("/job", async (req, res) => {
+  await getData();
+});
+
+const getData = async () => {
+  console.log("test");
+};
 
 app.listen("5000", (req, res) => {
   console.log("listening");
